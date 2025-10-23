c72f31e5244c5b5d39893b4e33d18098e280cd50
PabloGargalloSanz <pablogargallosanz@gmail.com>
2025-10-23 12:22:24 +0200

    Feat: middleware logger y logs en archivos por fecha

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 
+import logger from './middlewares/logger.js'
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 
 //Endpoint raiz
 app.get('/', (req,res) => {
---
+++ backend/middlewares/logger.js
@@ -0,0 +1,33 @@
+import fs from "fs";
+import path from "path";
+
+const logger = (req, res, next) => {
+    
+    const fecha = new Date().toISOString().split('T')[0];
+    const string = `[${new Date().toISOString()}] ${req.method} - ${req.url} - ${req.ip}`;
+
+    const direccion = path.join(process.cwd(), 'logs');
+    const nombreArchivo = path.join(direccion, fecha + 'log');
+
+    console.log(string);
+
+    //Crear carpeta logs si no esta creada
+    try {
+        fs.mkdirSync(direccion, { recursive: true});
+    } catch (error) {
+        console.error("Error al crear la carpeta logs: ", error);
+        return next();
+    }
+
+    //Escribe en el archivo log, nombreado por fecha, el log del server
+    fs.appendFile(nombreArchivo, string + '\n',
+        (error) => {
+            if(error) {
+                console.log("Error al escribir el archivo log:",error);
+            }
+        }
+    )
+
+};
+
+export default logger;
