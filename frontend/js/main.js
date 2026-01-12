32de1de486995ef4d3c03dcee394784dfaa8fc8d
PabloGargalloSanz <pablogargallosanz@gmail.com>
2026-01-12 23:40:51 +0100

    Feat: Almacenes html/css

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,9 +2,10 @@ const vistaLogin = document.getElementById("login-page");
 const vistaDashboard = document.getElementById("dashboard-page");
 const formLogin = document.getElementById("login-form");
 const btnLogout = document.getElementById("logout-btn");
+const emailDashboard = document.getElementById("display-email");
 
 // Comprobar si ya estamos logueados
-/*
+
 document.addEventListener("DOMContentLoaded", () => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -13,21 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
         mostrarLogin();
     }
 });
-*/
-
-function mostrarLogin() {
-    vistaDashboard.style.display = "none";
-    vistaLogin.style.display = "flex";
-    document.getElementById("email").value = "";
-    document.getElementById("password").value = "";
-}
-
-function mostrarDashboard() {
-    vistaLogin.style.display = "none";
-    vistaDashboard.style.display = "flex"; 
-    cargarCajones();
-}
-
 
 
 
@@ -71,7 +57,7 @@ formLogin.addEventListener("submit", async (e) => {
 */
 
 ////////////////////////////////////////////////////////////////
-// Código antiguo de login sin backend
+// Codigo antiguo de login sin backend
 //////////////////////////////////////////////////////////////
 
 const loginForm = document.getElementById('login-form');
@@ -81,7 +67,42 @@ loginForm.addEventListener('submit', (e) => {
         
     mostrarDashboard();
 });
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 
+function mostrarLogin() {
+    vistaDashboard.style.display = "none";
+    vistaLogin.style.display = "flex";
+    //emailDashboard.textContent = emailValue;
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+}
+
+
+function mostrarDashboard() {
+    vistaLogin.style.display = "none";
+    vistaDashboard.style.display = "flex"; 
+    cargarCajones();
+    renderAlmacenes();
+}
+
+//////////////////////////////////////////////////////////////
+/*datos prueba*/
+//////////////////////////////////////////////////////////////
+
+const almacenamientos = [
+    { id: 1, nombre: 'Congelador Cocina', localizacion: 'Cocina', ocupacion: 65 },
+    { id: 2, nombre: 'Arcón Garaje', localizacion: 'Garaje', ocupacion: 22 },
+    { id: 3, nombre: 'Nevera Pasillo', localizacion: 'Pasillo', ocupacion: 85 }
+];
+
+const inventario = [
+    { id: 101, nombre: 'Hamburguesas Vaca', tipo: 'Carne', cantidad: '4 uds', lugar: 'Cajón 1' },
+    { id: 102, nombre: 'Guisantes congelados', tipo: 'Verdura', cantidad: '1 kg', lugar: 'Cajón 2' },
+    { id: 103, nombre: 'Filetes de Pavo', tipo: 'Carne', cantidad: '500 g', lugar: 'Cajón 1' }
+];
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////        
 
 // Logout
 btnLogout.addEventListener("click", () => {
@@ -93,3 +114,32 @@ btnLogout.addEventListener("click", () => {
 async function cargarCajones() {
     console.log("Cargando cajones del backend...");
 }
+
+// randerizar almacenes
+function renderAlmacenes() {
+            const grid = document.getElementById('freezer-grid');
+            grid.innerHTML = ''; // Limpiar
+
+            almacenamientos.forEach(alm => {
+                const card = document.createElement('div');
+                card.className = 'card';
+                card.innerHTML = `
+                    <div class="almacen-contenedor">
+                        <div class="card-icon"></div>
+                        <span class="almacen-localizacion">
+                            ${alm.localizacion.toUpperCase()}
+                        </span>
+                    </div>
+                    <h3 class="almacen-nombre">${alm.nombre}</h3>
+                    <p class="almacen-ocupacion">Ocupación actual</p>
+                    <div class="progress-container">
+                        <div class="progress-bar" style="width: ${alm.ocupacion}%"></div>
+                    </div>
+                    <div class="status-row">
+                        <span style="color: ${alm.ocupacion > 80 ? '#ef4444' : '#024fa2'}">${alm.ocupacion}% lleno</span>
+                        <button class="btn-detail">Gestionar →</button>
+                    </div>
+                `;
+                grid.appendChild(card);
+            });
+        }
