5fb63b70c920d2933b5c6d1e5ffd98f78da1161d
PabloGargalloSanz <pablogargallosanz@gmail.com>
2026-03-26 13:23:25 +0100

    Feat: controlador y ruter patch lotes

--- backend/controllers/lotes.controller.js
+++ backend/controllers/lotes.controller.js
@@ -1,19 +1,20 @@
-import { crearLoteAlimentoService } from '../services/lotes.service.js';
+import { crearLoteAlimentoService, patchLoteService } from '../services/lotes.service.js';
 
-export const anadirAlimento = async (req, res) => {
+export const anadirAlimento = async (req, res, next) => {
+    const userId = req.userId;
+    const { 
+        alimento_nombre, alimento_tipo, cantidad, 
+        id_almacenamiento, posicion_cajon, fecha_caducidad, alimento_tamano 
+    } = req.body;
+
     try {
-        const userId = req.userId;
-        const { 
-            alimento_nombre, alimento_tipo, cantidad, 
-            id_almacenamiento, posicion_cajon, fecha_caducidad, alimento_tamano 
-        } = req.body;
 
         // validación e insercion errores
         if (!alimento_nombre || !alimento_tipo || !cantidad || !id_almacenamiento || !posicion_cajon || !fecha_caducidad || !alimento_tamano) {
-            req.action = 'REGISTER_INVENTARY_FAIL_VALIDATION'; 
-            return res.status(400).json({ 
-                error: "Todos los campos son obligatorios para registrar un alimento." 
-            });
+            const err = new Error("Datos incompletos para crear lote");
+            err.status = 400;
+            err.action = 'REGISTER_INVENTARY_FAIL_VALIDATION';    
+            return next(err); 
         }
         const loteData = req.body;
 
@@ -30,23 +31,60 @@ export const anadirAlimento = async (req, res) => {
         
         // error de cajon no encontrado
         if (error.message === "CAJON_NO_ENCONTRADO") {
+            error.status = 404;
             error.action = 'CAJON_NO_ENCONTRADO';
-            return res.status(404).json({ 
-                error: "El cajón especificado no existe o no te pertenece." 
-            });
         }
 
         // Capturamos el error del trigger de PostgreSQL
         if (error.message.includes('El cajón no tiene espacio suficiente')) {
             error.action = 'CAJON_SIN_ESPACIO';
-            return res.status(400).json({ 
-                error: "No hay espacio suficiente en el cajón seleccionado." 
-            });
+            error.status = 400;
+        }else{
+            error.status = 500;
+            error.action = 'CREATE_LOTE_SERVER_ERROR';
         }
         
-        error.action = 'SERVER_ERROR';
-        res.status(500).json({ 
-            error: "Error interno del servidor al guardar el lote." 
-        });
+        next(error);
     }
 };
+
+export const modificacionAlimento = async(req, res, next) =>{
+    const userId = req.userId;
+    const id = req.params;
+    const {cantidad, id_almacenamiento, posicion_cajon} = req.body;
+
+    try{
+        if(!cantidad || !id_almacenamiento || !posicion_cajon){
+            const err = new Error("Datos  incompletos para actualizar lote");
+            err.status = 400;
+            err.action = 'PATCH_INVENTARY_FAIL';    
+            return next(err); 
+        }
+
+        const loteEditado = await patchLoteService(userId, id, { 
+            cantidad, id_almacenamiento, posicion_cajon 
+        });
+
+        req.action = 'PATCH_INVENTARY_SUCCESS'; 
+        res.status(200).json({
+            mensaje: "Inventario actualizado correctamente",
+            lote: loteEditado
+        });
+
+    }catch (error) {
+        if (error.message.includes('Espacio insuficiente')) {
+            error.status = 400;
+            error.action = 'PATCH_INVENTARY_NO_SPACE';
+
+        } else if (error.message === "LOTE_NO_ENCONTRADO") {
+            error.status = 404;
+            error.action = 'PATCH_INVENTARY_NOT_FOUND';
+
+        } else {
+            error.status = 500;
+            error.action = 'PATCH_INVENTARY_SERVER_ERROR';
+        }
+
+        next(error);
+    }
+}
--- backend/routes/lotes.routes.js
+++ backend/routes/lotes.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
 
-import { anadirAlimento } from '../controllers/lotes.controller.js';
+import { anadirAlimento, modificacionAlimento } from '../controllers/lotes.controller.js';
 import {verifyToken} from '../middlewares/jwt.middleware.js'
 
 const router = Router();
 
 router.post('/lotes', verifyToken, anadirAlimento);
+router.patch('/lotes', verifyToken, modificacionAlimento);
 
 export default router;
